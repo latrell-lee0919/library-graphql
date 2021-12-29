7583741b87760cfe6464f6c81edfd6cd4c8a78ea
Latrell <lll4xy@virginia.edu>
2021-12-29 11:51:11 -0500

    exercise 8.7

--- library-backend.js
+++ library-backend.js
@@ -123,6 +123,10 @@ const typeDefs = gql`
           published: Int!
           genres: [String!]!
       ): Book
+      editAuthor(
+          name: String!
+          setBornTo: Int!
+      ): Author
   }
 `
 
@@ -166,6 +170,16 @@ const resolvers = {
         const authorObject = { name: args.author, id: uuid() }
         authors = authors.concat(authorObject)
         return book
+    },
+    editAuthor: (root, args) => {
+        const author = authors.find(a => a.name === args.name)
+        if (!author) {
+            return null
+        }
+
+        const updatedAuthor = { ...author, born: args.setBornTo }
+        authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+        return updatedAuthor
     }
   }
 }
